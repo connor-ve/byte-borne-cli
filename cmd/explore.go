07e89d41d82260ab1ae2779fc8a9a2eb4bc59cbd
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2024-06-17 23:42:11 -0400

    feat : added minor changes to explore page and stats page
    Imported Fatih Color Lib
    IMported Survey and reviewed Docs

--- cmd/explore.go
+++ cmd/explore.go
@@ -1,12 +1,16 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
 
+	lib "xcute/dark_terminal/lib"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -21,20 +25,47 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("explore called")
+		player, err := lib.ReadPlayerData("C:\\Users\\crvan\\dark_terminal\\database\\player_db.yaml")
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print("Current Status :  ")
+		color.Set(color.FgRed, color.Bold, color.Italic, color.Underline)
+		fmt.Printf("%s\n", player.Mode)
+		color.Unset() // Don't forget to unset
+
+		questions := []*survey.Question{
+			{
+				Name: "Explorer",
+				Prompt: &survey.Select{
+					Message: "How would you like to spend your time?",
+					Help:    "Setting current status of your player",
+					Options: []string{"Explore", "Rest", "Training"},
+					Default: "Explore",
+				},
+			},
+		}
+
+		ModeSet := ""
+
+		survey.Ask(questions, &ModeSet)
+
+		player.Mode = ModeSet
+
+		lib.Update(player)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(ModeSet)
+		runExplore()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(exploreCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// exploreCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// exploreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func runExplore() {
+	fmt.Println("Running Explore")
 }
--- cmd/stats.go
+++ cmd/stats.go
@@ -1,12 +1,14 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	lib "xcute/dark_terminal/lib"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -21,20 +23,32 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("stats called")
+		player, err := lib.ReadPlayerData("C:\\Users\\crvan\\dark_terminal\\database\\player_db.yaml")
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print("Name :  ")
+		color.Set(color.FgRed, color.Bold, color.Italic, color.Underline)
+		fmt.Printf("%s\n", player.Name)
+		color.Unset()
+
+		fmt.Print("Type :  ")
+		color.Set(color.FgRed, color.Bold, color.Italic, color.Underline)
+		fmt.Printf("%s\n", player.Class)
+		color.Unset()
+
+		fmt.Print("Tokens :  ")
+		color.Set(color.FgRed, color.Bold, color.Italic, color.Underline)
+		fmt.Printf("%d$\n", player.Experience)
+		color.Unset()
+
+		fmt.Print("Level :  ")
+		color.Set(color.FgRed, color.Bold, color.Italic, color.Underline)
+		fmt.Printf("%d\n", player.Level)
+		color.Unset()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(statsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// statsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// statsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
