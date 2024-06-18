d16c40f6b2039abea6fab549bc9f79c23823a95d
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2024-06-17 23:51:01 -0400

    feat : basic level up screen
    Minor additions setting up framework

--- cmd/level.go
+++ cmd/level.go
@@ -1,12 +1,15 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	lib "xcute/dark_terminal/lib"
 
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -21,20 +24,58 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("level called")
+		player, err := lib.ReadPlayerData("C:\\Users\\crvan\\dark_terminal\\database\\player_db.yaml")
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Print("Current Level :  ")
+		color.Set(color.FgRed, color.Bold, color.Italic, color.Underline)
+		fmt.Printf("%s\n", player.Level)
+		color.Unset() // Don't forget to unset
+
+		questions := []*survey.Question{
+			{
+				Name: "Level",
+				Prompt: &survey.Select{
+					Message: "How would you like to progress",
+					Help:    "Setting current status of your player",
+					Options: []string{"Attack", "Health", "Misc"},
+					Default: "Health",
+				},
+			},
+		}
+		levelup_type := ""
+
+		survey.Ask(questions, &levelup_type)
+
+		var options []string
+
+		switch levelup_type {
+		case "Attack":
+			options = []string{"Strength", "Agility", "Special"}
+		case "Health":
+			options = []string{"Increase Health", "Increase Block", "Increase Regen"}
+		case "Misc":
+			options = []string{"Update Scroll", "Update BackPack", "Demote"}
+		}
+
+		sub_questions := []*survey.Question{
+			{
+				Name: "Level",
+				Prompt: &survey.Select{
+					Message: "How would you like to progress",
+					Help:    "Setting current status of your player",
+					Options: options,
+				},
+			},
+		}
+		answer := ""
+
+		survey.Ask(sub_questions, &answer)
+		fmt.Println(answer)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(levelCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// levelCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// levelCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
